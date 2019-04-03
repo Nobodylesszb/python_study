ed73de74bc08362d0f2da8a23d7ad0c6a43cb226
Nobodylesszb <nobodylesszb@163.com>
2019-04-03 20:30:54 +0800

    实现队列和单列表输出

---
+++ python-offer/2.3/acheive_queue.py
@@ -0,0 +1,14 @@
+class MyQueue(object):
+    def __init__(self,)
+        self.stack = []
+        self.stack2 = []
+
+    def push(self,value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.stack2:
+            return self.stack2.pop()
+        while self.stack:
+            self.stack2.append(self.stack.pop())
+        return self.stack2.pop() if self.stack2 else u'队列为空'
---
+++ python-offer/2.3/打印单列表.py
@@ -0,0 +1,15 @@
+# 使用栈,可以使用列表模拟
+def print_links(links):
+    stack = []
+    while links:
+        stack.append(links.val)
+        links = links.next
+    while stack:
+        print stack.pop()
+
+# 使用递归
+
+def print_links_recursivion(links):
+    if links:
+        print_links_recursivion(links.next)
+        print links.val
