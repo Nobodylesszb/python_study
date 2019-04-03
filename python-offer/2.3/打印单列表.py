f1012066bb3de838149951fc3a70209d8ed25acb
Nobodylesszb <nobodylesszb@163.com>
2019-04-03 20:32:17 +0800

    修复队列的bug

--- python-offer/2.3/打印单列表.py
+++ python-offer/2.3/打印单列表.py
@@ -5,11 +5,11 @@ def print_links(links):
         stack.append(links.val)
         links = links.next
     while stack:
-        print stack.pop()
+        print (stack.pop())
 
 # 使用递归
 
 def print_links_recursivion(links):
     if links:
         print_links_recursivion(links.next)
-        print links.val
+        print (links.val)
