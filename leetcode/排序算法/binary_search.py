3c02d06340150c6efa5364891f0657e3c4655dc7
Nobodylesszb <nobodylesszb@163.com>
2019-04-12 09:19:01 +0800

    增加二分法的迭代版本

--- leetcode/排序算法/binary_search.py
+++ leetcode/排序算法/binary_search.py
@@ -12,3 +12,20 @@ def bin_search(list,val):
             low = mid + 1   
 
     return
+
+#### 迭代二分法
+
+def bin_search_rec(l,value,start=None,end =None):
+    start = start if start else 0
+    end = end if end else len(l)
+    if start <= end:
+        mid = (start+end)//2
+        if l[mid] == value:
+            return mid
+        elif l[mid]> value:
+            return bin_search_rec(l,value,start,mid-1)
+        else:
+            return bin_search_rec(l,value,mid+1,end)
+    else:
+        return None
+        
