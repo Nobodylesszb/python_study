5eca4cf4609a7610e95d0b8a1627c08dc0bd9687
Nobodylesszb <nobodylesszb@163.com>
2019-04-15 14:09:29 +0800

    增加有关链表的操作

---
+++ leetcode/listnode/reverse_node.py
@@ -0,0 +1,30 @@
+'''
+题目：输入一个链表，反转链表后，输出链表的所有元素。
+'''
+
+'''
+这一题还是蛮难的。链表的翻转，例如 1->2->3->4->5  ==>  5->4->3->2->1
+tmp = pHead.next   把当前节点的下一个节点保存下来
+pHead.next = pre   把前一个节点移到当前节点的下一个节点，因为要翻转节点，pre始终指向要反转节点的首节点
+pre = pHead        当前节点向后移一位
+pHead = tmp        把之前保存的下一个节点指针再给当前节点接着翻转
+33ms
+6016k
+'''
+
+# -*- coding:utf-8 -*-
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution:
+# @param {ListNode} head
+# @return {ListNode}
+    def reverseList(self, head):
+        prev = None
+        while head:
+            curr = head
+            head = head.next
+            curr.next = prev
+            prev = curr
+        return prev
---
+++ python-offer/array/find_one.py
@@ -0,0 +1,26 @@
+'''
+题目：在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+'''
+
+'''
+方法一： 循环迭代查找，不是最优
+284ms
+5760k
+'''
+
+class Solution:
+    # array 二维列表
+    def Find(self, target, array):
+        # write code here
+        if not array:
+            return
+        row = len(array)
+        col = len(array[0])
+
+        for i in range(row):
+            for j in range(col):
+                if target == array[i][j]:
+                    return True
+
+        return False
