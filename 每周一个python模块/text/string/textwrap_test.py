8879cbb8d953344d726b6786ebf19872a92e7a74
张博 <nobodylesszb@163.com>
2019-06-04 10:38:52 +0800

    新增textwrap功能

--- 每周一个python模块/text/string/textwrap_test.py
+++ 每周一个python模块/text/string/textwrap_test.py
@@ -53,3 +53,70 @@ situations where pretty-printing is desired.  It offers
 programmatic functionality similar to the paragraph wrapping
 or filling features found in many text editors.
 """
+
+dedented_text = textwrap.dedent(sample_text)
+wrapped = textwrap.fill(dedented_text,width=50)
+wrapped += '\n\nSecond paragraph after a blank line'
+final = textwrap.indent(wrapped, '> ')
+print('Quoted block:\n')
+print(final)
+
+"""
+output:
+Quoted block:
+
+>  The textwrap module can be used to format text
+> for output in situations where pretty-printing is
+> desired.  It offers programmatic functionality
+> similar to the paragraph wrapping or filling
+> features found in many text editors.
+
+> Second paragraph after a blank line
+"""
+
+## hanging indents
+
+dedented_text = textwrap.dedent(sample_text).strip()
+print(textwrap.fill(dedented_text,
+                    initial_indent='',
+                    subsequent_indent=' ' * 10,
+                    width=50,
+                    ))
+"""
+The textwrap module can be used to format text for
+          output in situations where pretty-
+          printing is desired.  It offers
+          programmatic functionality similar to
+          the paragraph wrapping or filling
+          features found in many text editors.
+"""
+
+## truncating long text
+
+dedented_text = textwrap.dedent(sample_text)
+original = textwrap.fill(dedented_text, width=50)
+
+print('Original:\n')
+print(original)
+
+shortened = textwrap.shorten(original, 100)
+shortened_wrapped = textwrap.fill(shortened, width=50)
+
+print('\nShortened:\n')
+print(shortened_wrapped)
+
+"""
+output:
+Original:
+
+ The textwrap module can be used to format text
+for output in situations where pretty-printing is
+desired.  It offers programmatic functionality
+similar to the paragraph wrapping or filling
+features found in many text editors.
+
+Shortened:
+
+The textwrap module can be used to format text for
+output in situations where pretty-printing [...]
+"""
