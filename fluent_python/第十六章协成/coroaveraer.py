d0d1527c6b9e5ad4cac15bfda5561bd1d2cb38b1
张博 <nobodylesszb@163.com>
2019-10-22 09:35:52 +0800

    迁移fluent_python到其他分支

--- fluent_python/第十六章协成/coroaveraer.py
+++
@@ -1,9 +0,0 @@
-def average():
-    total = 0
-    count = 0
-    average = None
-    while True:
-        term = yield average
-        total += term
-        count +=1
-        average = total/count
